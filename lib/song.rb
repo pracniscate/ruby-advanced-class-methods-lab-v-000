ae4eba6db9c65218f7f455611b657f40d69c48c5
Lorena <lorena.giddens@outlook.com>
2018-06-13 01:57:15 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,55 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    # initialize a song
+    song = self.new
+    # save the song to the @@all class var
+    @@all << song
+    # return the song that was created
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    # return a song instance with the name set as its name property
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    # return a song instance with that name set as its name property
+    song.name = name
+    # save song into the @@all class var
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    # return the matching instance of the song with that name
+    self.all.detect {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name) # to prevent duplicates
+    # either return a matching song instance with that name or create a new song and return the song instance
+    result = self.find_by_name(name)
+    result ? result : self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    @@all.sort_by { |song| song.name }
+  end
+
+  def self.new_from_filename(file_name)
+    # parse the relevant components
+    data = file_name.split(" - ")
+    song.artist_name = data[0]
+    name = data[1].split(".")
+    #return a new song instance with the song name set to song name and the artist name set to artist
+    song = self.new
+    song.name = name[0]
+    song
+  end
+
 end
